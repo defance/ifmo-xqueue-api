8b8c21b0355b85893ba686d45584a56f22bfc1be
Dmitry Ivanyushin <defance@gmail.com>
2016-03-11 18:44:14 +0300

    Починка инициализации

--- xqueue_api/xobject.py
+++ xqueue_api/xobject.py
@@ -17,7 +17,7 @@ class XObject(object):
 
     def __init__(self, api_response=None):
 
-        super(XSubmission, self).__init__(api_response=api_response)
+        super(XObject, self).__init__(api_response=api_response)
 
         if api_response is not None:
             self.init_api_response(json.loads(api_response))
